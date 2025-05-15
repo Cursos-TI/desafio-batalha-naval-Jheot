b487b05eff04d038efefa689d58cd21fe34ce910
Jheot <josueinfoctt@gmail.com>
2025-05-15 16:43:37 +0000

    iniciado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    int navio1Vertic[2], navio2Vertic[2];
+    int navio1Tamanho[4], navio2Tamanho[4];
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    int coluna [10] = {1 ,2 , 3, 4, 5, 6, 7, 8, 9, 10};
+    int tabuleiro[10][10] = 
+    {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+    {11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+    {21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
+    {31, 32, 33, 34, 35, 36, 37, 38, 39, 40},
+    {41, 42, 43, 44, 45, 46, 47, 48, 49, 50},
+    {51, 52, 53, 54, 55, 56, 57, 58, 59, 60},
+    {61, 62, 63, 64, 65, 66, 67, 68, 69, 70},
+    {71, 72, 73, 74, 75, 76, 77, 78, 79, 80},
+    {81, 82, 83, 84, 85, 86, 87, 88, 89, 90},
+    {91, 92, 93, 94, 95, 96, 97, 98, 99, 100}};
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("**BATALHA NAVAL***\n");
+    printf("%c ", linha[10]);
 
+
+   
     return 0;
 }
